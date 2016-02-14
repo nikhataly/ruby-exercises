e876a39df85cb2175868441e03039f7cb070f5ed
nikhataly <nikhataly@gmail.com>
2016-02-13 22:15:06 -0500

    Yay no nil

--- arrayfun.rb
+++ arrayfun.rb
@@ -2,19 +2,17 @@ require 'pry'
 
 b = [3, 20, 7, 40, 6, 90]
 
-puts b
+print b
 
 l = b.length
 index = 0
 l.times do	
-	binding.pry
 	if b[index] > 10
-		b.insert(l, b.delete_at(index))
+		b.insert(l-1, b.delete_at(index))
 	else
 		index += 1
 	end
 end
 
-puts b
-
+print b
 
